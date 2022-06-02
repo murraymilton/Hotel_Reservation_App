d26a27f3a7d14c41de38942901162fd806d7e269
murraymilton <murray.l.milton@outlook.com>
2022-06-02 17:53:02 +0200

    resolved the conflict in the interface for the rooms model class

--- hotelreservation/src/model/Room.java
+++ hotelreservation/src/model/Room.java
@@ -3,11 +3,10 @@ package model;
 public class Room implements IRoom{
     private String roomNumber;
     private double price;
-    private enum RoomType;
 
     @Override
     public String toString(){
-        return "Room Type:" + RoomType + "Room Number:" +
+        return "Room Type:" + "Room Number:" +
                 roomNumber + "\n Price:" + price;
     }
 }
